9e0f63a6851f0f81433caffb27119a35002381b7
Darkyy69 Laptop <radiasweetgirl812@gmail.com>
2024-04-11 08:55:13 +0200

    added DB (Comptoire)

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Comptoire',
     'rest_framework',
     'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -52,8 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
--- frontend/src/Pages/Vente/Components/HomeVent.jsx
+++ frontend/src/Pages/Vente/Components/HomeVent.jsx
@@ -6,42 +6,37 @@ import { Link,Outlet } from 'react-router-dom';
 
 function MonComposant() {
   return (
-<<<<<<<< HEAD:frontend/src/Pages/Vente/HomeVent.jsx
-    <div className='flex flex-row justify-center items-center gap-4'>
-      <Link to='/Vente/Comptoire' className='flex justify-center border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300 bg-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700'> 
-========
     <div className=''>
-    <div className='flex flex-row justify-center items-center gap-4 ml-56'>
+    <div className='flex flex-row justify-center items-center gap-4'>
       <Link to='/Vente/Comptoire' className='border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300 bg-gray-200'> 
->>>>>>>> 6776bcc2cd5737130b9b1526dab7043274f9d5ae:frontend/src/Pages/Vente/Components/HomeVent.jsx
         <div className='flex flex-col justify-center items-center gap-9'>
           <h1 className='text-2xl font-bold'>Comptoire</h1>
           <FaShoppingCart size={94} />
         </div>
       </Link>
 
-      <Link to='/Vente/ListBons' className='flex justify-center border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700'>
+      <Link to='/Vente/ListBons' className='border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200'>
         <div className='flex flex-col justify-center items-center gap-9'>
           <h1 className='text-2xl font-bold'>ListBons</h1>
           <IoCard size={94} />
         </div>
       </Link>
 
-      <Link to='/Vente/GestionCLients' className='flex justify-center border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700'>
+      <Link to='/Vente/GestionCLients' className='border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200'>
         <div className='flex flex-col justify-center items-center gap-9'>
           <h1 className='text-2xl font-bold'>GestionClients</h1>
           <BiGroup size={94} />
         </div>
       </Link>
 
-      <Link to='/Vente/PaymentClient' className='flex justify-center border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700'>
+      <Link to='/Vent/PaymentClient' className='border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200'>
         <div className='flex flex-col justify-center items-center gap-9'>
           <h1 className='text-2xl font-bold'>PaymentClient</h1>
           <FaMoneyBillAlt size={94} />
         </div>
       </Link> 
 
-      <Link to='/Vente/Vendre' className='flex justify-center border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700'>
+      <Link to='/Vente/Vendre' className='border h-56 w-56 rounded-md shadow-md transition-transform transform hover:scale-105 hover:bg-blue-300  bg-gray-200'>
         <div className='flex flex-col justify-center items-center gap-9'>
           <h1 className='text-2xl font-bold'>Vendre</h1>
           <FaShoppingCart size={94} />
